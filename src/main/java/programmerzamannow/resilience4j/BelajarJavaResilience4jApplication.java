da714d892c2b3d8334dfc56053d8d452fa98cfd1
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-09-10 10:45:53 +0700

    Create Project Belajar Java Resilience4J

---
+++ src/main/java/programmerzamannow/resilience4j/BelajarJavaResilience4jApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaResilience4jApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BelajarJavaResilience4jApplication.class, args);
+  }
+
+}
---
+++ src/test/java/programmerzamannow/resilience4j/BelajarJavaResilience4jApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.resilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaResilience4jApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
