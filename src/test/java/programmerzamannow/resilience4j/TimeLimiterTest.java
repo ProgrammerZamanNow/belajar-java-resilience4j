fd80e1a9f969cbb164e41742cfa62e89c2185260
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-09-15 20:49:44 +0700

    Time Limiter Registry

--- src/test/java/programmerzamannow/resilience4j/TimeLimiterTest.java
+++ src/test/java/programmerzamannow/resilience4j/TimeLimiterTest.java
@@ -2,6 +2,7 @@ package programmerzamannow.resilience4j;
 
 import io.github.resilience4j.timelimiter.TimeLimiter;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -44,6 +45,25 @@ public class TimeLimiterTest {
         .build();
 
     TimeLimiter timeLimiter = TimeLimiter.of("pzn", config);
+    Callable<String> callable = TimeLimiter.decorateFutureSupplier(timeLimiter, () -> future);
+
+    callable.call();
+  }
+
+  @Test
+  void timeLimiterRegistry() throws Exception {
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    Future<String> future = executorService.submit(() -> slow());
+
+    TimeLimiterConfig config = TimeLimiterConfig.custom()
+        .timeoutDuration(Duration.ofSeconds(10))
+        .cancelRunningFuture(true)
+        .build();
+
+    TimeLimiterRegistry registry = TimeLimiterRegistry.ofDefaults();
+    registry.addConfiguration("config", config);
+
+    TimeLimiter timeLimiter = registry.timeLimiter("pzn", "config");
     Callable<String> callable = TimeLimiter.decorateFutureSupplier(timeLimiter, () -> future);
 
     callable.call();
