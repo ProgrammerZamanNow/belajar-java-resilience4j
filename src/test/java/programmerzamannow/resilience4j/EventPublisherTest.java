482a9c066ff6fd1bd3200e3cc5b379a0eb1be508
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-09-15 22:05:59 +0700

    Event Publisher

---
+++ src/test/java/programmerzamannow/resilience4j/EventPublisherTest.java
@@ -0,0 +1,33 @@
+package programmerzamannow.resilience4j;
+
+import io.github.resilience4j.retry.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+@Slf4j
+public class EventPublisherTest {
+
+  @Test
+  void retry() {
+    Retry retry = Retry.ofDefaults("pzn");
+    retry.getEventPublisher().onRetry(event -> {
+      log.info("try to retry");
+    });
+
+    try {
+      Supplier<String> supplier = Retry.decorateSupplier(retry, () -> hello());
+      supplier.get();
+    } catch (Exception e) {
+      System.out.println(retry.getMetrics().getNumberOfFailedCallsWithRetryAttempt());
+      System.out.println(retry.getMetrics().getNumberOfFailedCallsWithoutRetryAttempt());
+      System.out.println(retry.getMetrics().getNumberOfSuccessfulCallsWithRetryAttempt());
+      System.out.println(retry.getMetrics().getNumberOfSuccessfulCallsWithoutRetryAttempt());
+    }
+  }
+
+  private String hello() {
+    throw new IllegalArgumentException("Ups");
+  }
+}
