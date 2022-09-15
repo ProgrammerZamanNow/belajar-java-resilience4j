d2f2f9658b21824e66f2b2c1ee7f8b2685b0c84a
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-09-15 21:09:47 +0700

    Circuit Breaker

---
+++ src/test/java/programmerzamannow/resilience4j/CircuitBreakerTest.java
@@ -0,0 +1,49 @@
+package programmerzamannow.resilience4j;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class CircuitBreakerTest {
+
+  public void callMe(){
+    log.info("Call Me");
+    throw new IllegalArgumentException("Ups");
+  }
+
+  @Test
+  void circuitBreaker() {
+    CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("pzn");
+
+    for (int i = 0; i < 200; i++) {
+      try {
+        Runnable runnable = CircuitBreaker.decorateRunnable(circuitBreaker, () -> callMe());
+        runnable.run();
+      }catch (Exception e){
+        log.error("Error : {}", e.getMessage());
+      }
+    }
+  }
+
+  @Test
+  void circuitBreakerConfig() {
+    CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+        .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+        .failureRateThreshold(10f)
+        .slidingWindowSize(10)
+        .minimumNumberOfCalls(10)
+        .build();
+    CircuitBreaker circuitBreaker = CircuitBreaker.of("pzn", config);
+
+    for (int i = 0; i < 200; i++) {
+      try {
+        Runnable runnable = CircuitBreaker.decorateRunnable(circuitBreaker, () -> callMe());
+        runnable.run();
+      }catch (Exception e){
+        log.error("Error : {}", e.getMessage());
+      }
+    }
+  }
+}
