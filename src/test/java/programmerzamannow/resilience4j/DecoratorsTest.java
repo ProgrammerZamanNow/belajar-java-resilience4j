b73fc2033127f6e152e2dc1a885db860f57d01a1
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-09-15 21:22:41 +0700

    Decorators

---
+++ src/test/java/programmerzamannow/resilience4j/DecoratorsTest.java
@@ -0,0 +1,47 @@
+package programmerzamannow.resilience4j;
+
+import io.github.resilience4j.decorators.Decorators;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+@Slf4j
+public class DecoratorsTest {
+
+  @SneakyThrows
+  public void slow() {
+    log.info("Slow");
+    Thread.sleep(1_000L);
+    throw new IllegalArgumentException("Error");
+  }
+
+  @Test
+  void decorators() throws InterruptedException {
+    RateLimiter rateLimiter = RateLimiter.of("pzn-ratelimiter", RateLimiterConfig.custom()
+        .limitForPeriod(5)
+        .limitRefreshPeriod(Duration.ofMinutes(1))
+        .build());
+
+    Retry retry = Retry.of("pzn-retry", RetryConfig.custom()
+        .maxAttempts(10)
+        .waitDuration(Duration.ofMillis(10))
+        .build());
+
+    Runnable runnable = Decorators.ofRunnable(() -> slow())
+        .withRetry(retry)
+        .withRateLimiter(rateLimiter)
+        .decorate();
+
+    for (int i = 0; i < 100; i++) {
+      new Thread(runnable).start();
+    }
+
+    Thread.sleep(10_000L);
+  }
+}
